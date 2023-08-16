f9fe9315afee085941a792ae5f24b9ff6f6dc01f
Roman <romanmalysh@romans-macbook-air.home>
2023-08-16 17:16:39 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -23,6 +23,7 @@ export const App: React.FC = () =>  {
     event.preventDefault();
     const value = `${search.replace(/\s/g, '')}`
     setIsUsers(false);
+    setIsError(false);
     setSearch('');
     setIsLoading(true);
     const res =  await getUsers(value);
@@ -76,7 +77,7 @@ export const App: React.FC = () =>  {
             type='search'
             id='default-search'
             className='block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
-            placeholder='Search Mockups, Logos...'
+            placeholder='Provide a user name..'
             required
             value={search}
             onChange={handleChange}
